6278c48635042e97864c0e5ded5e793831e91aff
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2025-10-09 20:04:55 +0500

    fix: update canonical URL in About page metadata and clean up import order in Portfolio page

--- src/app/(app)/about/page.tsx
+++ src/app/(app)/about/page.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     url: `${baseUrl}/about`,
   },
   alternates: {
-    canonical: `${baseUrl}/services`,
+    canonical: `${baseUrl}/about`,
   },
 };
 
--- src/app/(app)/portfolio/page.tsx
+++ src/app/(app)/portfolio/page.tsx
@@ -1,9 +1,9 @@
+import ComingSoonSection from "@/components/section/portfolio/ComingSoonSection";
+import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "Portfolio",
   description: "Our Innovative Projects Are Coming Soon!",
 };
-import ComingSoonSection from "@/components/section/portfolio/ComingSoonSection";
-import { Metadata } from "next";
 
 const PortfolioPage = () => {
   return (
