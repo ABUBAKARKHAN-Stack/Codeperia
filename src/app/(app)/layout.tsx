d0231265b110f47c82b6367ba4fa70228fe860fc
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2025-10-14 19:25:51 +0500

    feat: add metadata for SEO optimization in layout component

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,6 +1,19 @@
 import { Footer, Navbar } from "@/components/layout";
 import ScrollToTopBtn from "@/components/ui/scroll-to-top-btn";
+import { baseUrl, brandName } from "@/constants/constants";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  robots: "index, follow",
+  openGraph: {
+    url: `${baseUrl}/`,
+    siteName: brandName,
+  },
+  alternates: {
+    canonical: `${baseUrl}/`,
+  },
+}
 
 const layout = ({
   children,
