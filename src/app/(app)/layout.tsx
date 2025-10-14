3fc93ea2cd43c2a5b1c1963dbca9c136a6734e12
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2025-10-14 19:31:39 +0500

    trying to fix og images

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -5,15 +5,64 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
+  title: {
+    default: `${brandName} | Software Development Company`,
+    template: `%s | ${brandName}`,
+  },
+  description:
+    "We are a forward-thinking tech startup creating modern, scalable web and software solutions that combine creativity, technology, and strategy for success.",
+  icons: {
+    icon: [
+      { url: "/assets/favicons/favicon.ico", type: "image/png" },
+      { url: "/assets/favicons/favicon.svg", type: "image/svg+xml" },
+      {
+        url: "/assets/favicons/favicon-32X32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+      {
+        url: "/assets/favicons/favicon-16X16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+    ],
+    apple: [
+      {
+        url: "/assets/favicons/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+      },
+    ],
+  },
+  keywords: [
+    "tech startup",
+    "codeperia",
+    "digital tech agency",
+    "software development",
+    "custom web applications",
+    "ui-ux",
+    "ui",
+    "ux",
+    "devops and automation",
+    "devops",
+    "automation",
+    "tech agency",
+    "modern digital solutions",
+    "scalable web apps",
+    "scalable mobile apps",
+  ],
+  creator: brandName,
+  publisher: brandName,
   robots: "index, follow",
-  openGraph: {
-    url: `${baseUrl}/`,
-    siteName: brandName,
-  },
+  category: "technology",
   alternates: {
-    canonical: `${baseUrl}/`,
+    canonical: baseUrl,
   },
-}
+  openGraph: {
+    siteName: brandName,
+    url: baseUrl,
+  },
+};
 
 const layout = ({
   children,
