6f9e2164f1e072ac4e1f2b5f5aef8557338512dc
ABUBAKARKHAN-Stack <abubakar123456654123@gmail.com>
2026-01-02 03:47:05 +0500

    fix: update robots.txt rules to specify user agents

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,9 +3,28 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-    },
+    rules: [
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+      {
+        userAgent: "Twitterbot",
+        allow: "/",
+      },
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   };
 }
